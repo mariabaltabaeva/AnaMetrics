3f8c6bbef8842ba072de219b6c9c2aaaafa1819a
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-07-27 23:42:49 -0400

    US-5:completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  namespace :api, default: { format: :json } do
+  match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
+
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
